cc11a5895557d2f4b81e255123f55bbf7689c4bb
Prashant Kumar <prashantkumar102369@gmail.com>
2026-07-19 23:31:41 +0530

    Time: 0 ms (100%), Space: 22.7 MB (19.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         if(nums.size() == 0){return 0;}
 
         int unq_pos = 0;
-        for(int i=0; i<nums.size(); i++){
+        for(int i=1; i<nums.size(); i++){
             if(nums[i] != nums[unq_pos]){unq_pos++; nums[unq_pos] = nums[i];}
         }
         return unq_pos+1;
